04af23a30266831ca9f1619383612b139a91a49e
enncy <877526278@qq.com>
2024-01-23 16:39:24 +0800

    update api

--- src/utils.ts
+++ src/utils.ts
@@ -6,61 +6,71 @@ export const METADATA_PARAMETER_KEY = 'metadata:parameter';
 
 /**
  * 设置类装饰器的元数据
- * @param name 		装饰器名字
+ * @param decorator_name 		装饰器名字
  * @param target 	类
  * @param value 	值
  */
-export function setClassMetadata(name: string, target: Function, value: any) {
-	Reflect.defineMetadata(`${METADATA_CLASS_KEY}:${name.toLowerCase()}`, value, target);
+export function setClassMetadata(decorator_name: string | { name: string }, target: Function, value: any) {
+	const _name = typeof decorator_name === 'string' ? decorator_name : decorator_name.name;
+	Reflect.defineMetadata(`${METADATA_CLASS_KEY}:${_name.toLowerCase()}`, value, target);
 }
 
 /**
  * 设置属性装饰器的元数据
- * @param name 		装饰器名字
+ * @param decorator_name 		装饰器名字
  * @param target	类
  * @param key 		属性
  * @param value		值
  */
-export function setPropertyMetadata(name: string, target: Object, key: string | symbol, value: any) {
-	Reflect.defineMetadata(`${METADATA_PROPERTY_KEY}:${name.toLowerCase()}`, value, target, key);
+export function setPropertyMetadata(
+	decorator_name: string | { name: string },
+	target: Object,
+	key: string | symbol,
+	value: any
+) {
+	const _name = typeof decorator_name === 'string' ? decorator_name : decorator_name.name;
+	Reflect.defineMetadata(`${METADATA_PROPERTY_KEY}:${_name.toLowerCase()}`, value, target, key);
 }
 
 /**
  * 设置参数装饰器的元数据
- * @param name 		装饰器名字
+ * @param decorator_name 		装饰器名字
  * @param target 	类
  * @param key 		属性
  * @param value 	值
  */
 export function setParameterMetadata(
-	name: string,
+	decorator_name: string | { name: string },
 	target: Object,
 	key: string | symbol | undefined,
 	parameter_index: number,
 	value: any
 ) {
 	if (key) {
-		Reflect.defineMetadata(`${METADATA_PARAMETER_KEY}:${parameter_index}:${name.toLowerCase()}`, value, target, key);
+		const _name = typeof decorator_name === 'string' ? decorator_name : decorator_name.name;
+		Reflect.defineMetadata(`${METADATA_PARAMETER_KEY}:${parameter_index}:${_name.toLowerCase()}`, value, target, key);
 	}
 }
 
 /**
  * 获取类装饰器的元数据
- * @param name 		装饰器名称
+ * @param decorator_name 		装饰器名称
  * @param target 	类
  */
-export function getClassMetadata(name: string, target: Function) {
-	return Reflect.getMetadata(`${METADATA_CLASS_KEY}:${name.toLowerCase()}`, target);
+export function getClassMetadata(decorator_name: string | { name: string }, target: Function) {
+	const _name = typeof decorator_name === 'string' ? decorator_name : decorator_name.name;
+	return Reflect.getMetadata(`${METADATA_CLASS_KEY}:${_name.toLowerCase()}`, target);
 }
 
 /**
  * 获取属性装饰器的元数据
- * @param name    装饰器名称
+ * @param decorator_name    装饰器名称
  * @param target  类
  * @param key  	  属性
  */
-export function getPropertyMetadata(name: string, target: Object, key: string | symbol) {
-	return Reflect.getMetadata(`${METADATA_PROPERTY_KEY}:${name.toLowerCase()}`, target, key);
+export function getPropertyMetadata(decorator_name: string | { name: string }, target: Object, key: string | symbol) {
+	const _name = typeof decorator_name === 'string' ? decorator_name : decorator_name.name;
+	return Reflect.getMetadata(`${METADATA_PROPERTY_KEY}:${_name.toLowerCase()}`, target, key);
 }
 
 /**
@@ -69,6 +79,12 @@ export function getPropertyMetadata(name: string, target: Object, key: string | 
  * @param target   类
  * @param key 	   属性
  */
-export function getParameterMetadata(name: string, target: Object, key: string | symbol, parameter_index: number) {
-	return Reflect.getMetadata(`${METADATA_PARAMETER_KEY}:${parameter_index}:${name.toLowerCase()}`, target, key);
+export function getParameterMetadata(
+	decorator_name: string | { name: string },
+	target: Object,
+	key: string | symbol,
+	parameter_index: number
+) {
+	const _name = typeof decorator_name === 'string' ? decorator_name : decorator_name.name;
+	return Reflect.getMetadata(`${METADATA_PARAMETER_KEY}:${parameter_index}:${_name.toLowerCase()}`, target, key);
 }
